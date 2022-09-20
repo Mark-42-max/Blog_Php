a3d592e7d54d973ce698a3bb9e403e0fd9612617
Soham Das <79685489+Mark-42-max@users.noreply.github.com>
2022-09-20 14:43:21 +0530

    seeds created

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +17,53 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::truncate();
+        Category::truncate();
+        Post::truncate();
+
+        $user = User::factory(1)->create();
+
+        $family = Category::create([
+            'name' => 'Family',
+            'slug' => 'family',
+        ]);
+
+        $work = Category::create([
+            'name' => 'Work',
+            'slug' => 'work',
+        ]);
+
+        $hobby = Category::create([
+            'name' => 'Hobby',
+            'slug' => 'hobby',
+        ]);
+
+        Post::create([
+            'user_id' => 1,
+            'category_id' => 1,
+            'title' => 'My Family',
+            'slug' => 'my-family',
+            'excerpt' => 'My Family is the best',
+            'body' => 'My Family is the best reset your application successful_response',
+        ]);
+
+        Post::create([
+            'user_id' => 2,
+            'category_id' => 2,
+            'title' => 'My Work',
+            'slug' => 'my-work',
+            'excerpt' => 'My Work is the best',
+            'body' => 'My Work is the best reset your application successful_response',
+        ]);
+
+        Post::create([
+            'user_id' => 3,
+            'category_id' => 3,
+            'title' => 'My Hobby',
+            'slug' => 'my-hobby',
+            'excerpt' => 'My Hobby is the best',
+            'body' => 'My Hobby is the best reset your application successful_response',
+        ]);
     }
 }
