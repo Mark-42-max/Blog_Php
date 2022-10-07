7a2e9820d585a35b0ce484dca28c5c49dca003b5
itchSoham <114210239+itchSoham@users.noreply.github.com>
2022-10-07 13:50:12 +0530

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Category;
+use App\Models\Message;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Database\Seeder;
@@ -21,12 +22,22 @@ class DatabaseSeeder extends Seeder
 
         $user1 = User::factory()->create();
         $user2 = User::factory()->create();
-        Post::factory(5)->create([
+        $user3 = User::factory()->create();
+        $user4 = User::factory()->create();
+        Post::factory(3)->create([
             'user_id' => $user1->id,
         ]);
 
-        Post::factory(5)->create([
+        Post::factory(3)->create([
             'user_id' => $user2->id,
+        ]);
+
+        Post::factory(3)->create([
+            'user_id' => $user3->id,
+        ]);
+
+        Post::factory(3)->create([
+            'user_id' => $user4->id,
         ]);
     }
 }
